8bd2422c3d9f7dbea95f6bdd252643fe5ead61ac
veeresh <veeresh@railsfactory.org>
2014-07-03 23:46:36 +0530

    one failure occurs

--- roman/lib/roman.rb
+++ roman/lib/roman.rb
@@ -1,12 +1,17 @@
 class Roman
 
   def self.convert(number)
-    if number ==1
-    'I'
-  else
-    'II'
-  end
-  
+    if number ==4
+    'IV'
+  elsif number ==5
+    'V'
+  elsif number ==6
+  'VI'
+ elsif number == 9 
+'IV'
+elsif numeber ==10
+'X'
+ end 
   end
 
 end  
--- roman/test/roman_test.rb
+++ roman/test/roman_test.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/roman'
+require '../lib/roman'
 require 'test/unit'
 
 
@@ -10,8 +10,8 @@ class TestRoman < Test::Unit::TestCase
 
 
   def test_one
-    assert_equal 'I',Roman.convert(1)
-    assert_equal 'II',Roman.convert(2)
+    assert_equal 'IV',Roman.convert(4)
+    assert_equal 'V',Roman.convert(5)
     assert_equal 'III',Roman.convert(3)
     assert_equal 'IV',Roman.convert(4)
   end
