5a68edda950ba40480c75c5a0e0f2ed30bc79924
Senthil Nayagam <senthil@railsfactory.com>
2014-07-03 16:08:15 +0530

    added a scaffold code

---
+++ roman/readme.txt
@@ -0,0 +1,15 @@
+
+
+
+Roman	Hindu-Arabic
+1	I
+4	IV
+5	V
+6	VI
+7	VII
+9	IX
+10	X
+50	L
+100	C
+500	D
+1000	M
---
+++ roman/test/roman_test.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/roman'
+require 'test/unit'
+
+
+class TestRoman < Test::Unit::TestCase
+
+  def test_hello
+    assert true
+  end
+
+
+
+
+end  
