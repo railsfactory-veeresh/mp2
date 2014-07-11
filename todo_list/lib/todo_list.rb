79189c1d4036daf656003758898c07c899f06cb3
veeresh <veeresh@railsfactory.org>
2014-07-11 14:30:27 +0530

    the test passes for all possible values

--- todo_list/lib/todo_list.rb
+++ todo_list/lib/todo_list.rb
@@ -1,70 +1,70 @@
 class Todolist
-@todo=[]
-@pending=[]
-@completed=[]
-@add=[]
-def self.initialize(filename)
-@filename=filename
+$todo=[]
+$pending=[]
+$completed=[]
+$add=[]
+def initialize(filename)
+$filename=filename
 end
-def self.empty
-@todo.clear
-@pending.clear
-@completed.clear
-@add.clear
+def empty
+$todo.clear
+$pending.clear
+$completed.clear
+$add.clear
 return 0
 end
 
 
-def self.add(item)
-@add << item
-@pending << item
-return @add.size
+def add(item)
+$add << item
+$pending << item
+return $add.size
 end
 
-def self.pending
-return @pending.size
+def pending
+return $pending.size
 end
 
-def self.complete(linenumber)
-@completed << @pending[linenumber-1]
-@pending.delete_at(linenumber - 1)
-return @completed.size
+def complete(linenumber)
+$completed << $pending[linenumber-1]
+$pending.delete_at(linenumber - 1)
+return $completed.size
 end
 
 def completed
-return @completed.size
+return $completed.size
 end
 
 
-def self.list
-@todo = @pending + @completed
-return @todo.size
+def list
+$todo = $pending + $completed
+return $todo.size
 end
 
-def self.delete(linenumber)
-@completed.delete_at(linenumber - 1)
+def delete(linenumber)
+$completed.delete_at(linenumber - 1)
 
-return @completed.size
+return $completed.size
 end
 
 
-def self.modify(linenumber,item)
-@pending.delete_at(linenumber-1)
-@pending << item
-return @pending.size
+def modify(linenumber,item)
+$pending.delete_at(linenumber-1)
+$pending << item
+return $pending.size
 end
 
-def self.empty
-a = @todo.clear
+def empty
+a = $todo.clear
 return a.size
 end
 
-def self.show_pending(linenumber)
-return @pending[linenumber-1]
+def show_pending(linenumber)
+return $pending[linenumber-1]
 end
 
-def self.show_completed(linenumber)
-return @completed[linenumber-1]
+def show_completed(linenumber)
+return $completed[linenumber-1]
 end
 end
 
--- todo_list/test/todo_list_test.rb
+++ todo_list/test/todo_list_test.rb
@@ -3,40 +3,40 @@ require 'test/unit'
 
 
 class Testcase < Test::Unit::TestCase
- 
+ $object=Todolist.new("veer.txt")
 
 
   def test_add1
-  assert_equal 0,Todolist.empty
-  assert_equal 0,Todolist.list
-  assert_equal 0,Todolist.pending
+  assert_equal 0,$object.empty
+  assert_equal 0,$object.list
+  assert_equal 0,$object.pending
   
   end 
   def test_add2
-   assert_equal 0,Todolist.empty
-   assert_equal 1,Todolist.add("hello")
-   assert_equal 1,Todolist.pending
-   assert_equal 1,Todolist.list
-   assert_equal "hello",Todolist.show_pending(1)
+   assert_equal 0,$object.empty
+   assert_equal 1,$object.add("hello")
+   assert_equal 1,$object.pending
+   assert_equal 1,$object.list
+   assert_equal "hello",$object.show_pending(1)
 end
 
 def test_add3
-assert_equal 2,Todolist.add("yellow")
-assert_equal 3,Todolist.add("blue")
-assert_equal 1,Todolist.complete(1)
-assert_equal 2,Todolist.pending
-assert_equal "yellow",Todolist.show_pending(1)
-assert_equal "hello",Todolist.show_completed(1)
+assert_equal 2,$object.add("yellow")
+assert_equal 3,$object.add("blue")
+assert_equal 1,$object.complete(1)
+assert_equal 2,$object.pending
+assert_equal "yellow",$object.show_pending(1)
+assert_equal "hello",$object.show_completed(1)
 end
 def test_add4
-assert_equal 4,Todolist.add("orange")
-assert_equal 5,Todolist.add("pink")
-assert_equal 2,Todolist.complete(2)
-assert_equal 3,Todolist.modify(2,"hi")
+assert_equal 4,$object.add("orange")
+assert_equal 5,$object.add("pink")
+assert_equal 2,$object.complete(2)
+assert_equal 3,$object.modify(2,"hi")
 end
 def test_add5
-assert_equal 6,Todolist.add("gray")
-
+assert_equal 6,$object.add("gray")
+assert_equal 1,$object.delete(1)
 
 end
 end
