7268514cee1d6cafb341c9e8d0d807d7a4a56599
veeresh <veeresh@railsfactory.org>
2014-07-14 14:45:45 +0530

    test passes for load from and save to file

--- todo_list/lib/todo_list.rb
+++ todo_list/lib/todo_list.rb
@@ -1,75 +1,117 @@
 class Todolist 
-attr_accessor :filename
-  def initialize(filename)
-   @filename = filename
-   @pending = []
-   @todo = []
-   @completed = []
-  end
+# attr_accessor :filename
+        def initialize(filename)
+        @filename = filename
+        @pending = []
+        @todo = []
+	@completed = []
+ 	end
 
-  def pending
-    return @pending
-  end
- 
-  def list
-  @todo = @pending + @completed
-  @todo
-  end
+	def pending
+          return @pending
+	end
 
-  def completed
-   @completed
-  end
+	def list
+          @todo = @pending + @completed
+           @todo
+        end
 
-  def add(title)
-  @pending << title
-  @pending
-  end
+	def completed
+          @completed
+	end
 
-  def complete(num)
-  @completed << @pending[num - 1]
-  @pending.delete_at(num - 1)
-  @completed.size
-  end
+	def add(title)
+          @pending << title
+          @pending
+	end
 
-  def delete(num)
-  @completed.delete_at(num - 1)
-  @completed
-  end
+	def complete(num)
+	@completed << @pending[num - 1]
+        @pending.delete_at(num - 1)
+	return @completed
+	end
 
-  def empty
-  @pending = []
-  @completed = []
-  @todo = []
-  return true
- end
+	
+	def delete(num)
+	@completed.delete_at(num - 1)
+	@completed
+	end
 
-  def modify(num,title)
-    @pending[num - 1] = title
-    return @pending[num - 1]
-  end
+	def empty
+	@pending = []
+	@completed = []
+	@todo = []
+	return true
+	end
 
-  def show_pending(num)
-    return @pending[num - 1]
-  end
+	def modify(num,title)
+	@pending[num - 1] = title
 
-  def show_completed(num)
-    return @completed[num - 1]
-  end
-  def save
-    f = File.open(@filename, "w")
-    str =""
-    str = @todo.join("\n")
-    f.write(str)
-    f.close
-   end
+	return @pending[num - 1]
+	end
+
+	def show_pending(num)
+ 	return @pending[num - 1]
+	end
+
+	def show_completed(num)
+         
+ 	return @completed[num - 1]
+	end
+
+        def save
+        end
+      
+       def load1
+        f = File.read(@filename)
+        f.each_line do |line|
+          if line.match('#undone')
+            @pending << line
+         else
+            @completed << line
+      end
+    end
+        return @pending
+        
+     end
+   
+      def complete9(num)
+        @completed << @pending[num - 1]
+        @completed.each do |s|
+          s.gsub!(/#undone/,"#done")
+           end
+         @pending.delete_at(num-1)
+      end
 
 
-  def load1
-   f = File.open(@filename,'r')
-   f.each_line { |line| @todo << line }
-   @completed = @todo.select {|c| c.match("#done")}
-   @pending = @todo - @completed
-  end
+      def save
+        f= File.open(@filename,"w")
+        str = ""
+        str = @todo
+        f.puts(str)
+        f.close
+        return str
+         
+      end 
+    
+     def save_to_file(filename)
+       f = File.open(filename,"w")
+        str = ""
+       str = @todo
+       f.puts(str)
+        f.close
+     end
+           
+    def load_from_file(filename)
+       f= File.open(filename,"r+")   
+         f.each_line do |line|
+          if line.match('#undone')
+            @pending << line
+         else
+            @completed << line
+      end
+    end
+        return @pending
 end
-
+end
 
--- todo_list/test/todo_list_test.rb
+++ todo_list/test/todo_list_test.rb
@@ -1,4 +1,4 @@
-require '../lib/todo_list.rb'
+require'../lib/todo_list.rb'
 require 'test/unit'
 class TestTodo < Test::Unit::TestCase
  
@@ -23,8 +23,6 @@ end
   assert_equal 0,@a.list.size
  end
 
-
-
   def test_add1
    @a.empty
    @a.add("one")
@@ -33,94 +31,147 @@ end
    assert_equal 0,@a.completed.size
  end
 
-
-
   def test_add2
-@a.empty
-@a.add("one")
+   @a.empty
+   @a.add("one")
    @a.add("two")
    assert_equal 2,@a.list.size
    assert_equal 2,@a.pending.size
    assert_equal 0,@a.completed.size
  end
-
-
-
-
- def test_add3
-   @a.empty
-   @a.add("write")
-   @a.add("read")
-   assert_equal 2,@a.list.size
-   assert_equal 2,@a.pending.size
-   assert_equal 1,@a.complete(1)
-   assert_equal 1,@a.pending.size
-    assert_equal 1,@a.completed.size
- end
-
-
-  
- def test_complete
-# precondition
-@a.empty
-@a.add("one")
-#before state
-assert_equal 1,@a.pending.size
-assert_equal 0,@a.completed.size
-assert_equal 1,@a.list.size
-
-
-#action
+ 
+  def test_complete
+  # precondition
+  @a.empty
+  @a.add("one")
+  #before state
+  assert_equal 1,@a.pending.size
+  assert_equal 0,@a.completed.size
+  assert_equal 1,@a.list.size
+   #action
    @a.complete(1)
+   #after 
+   assert_equal 0,@a.pending.size
+   assert_equal 1,@a.completed.size
+   assert_equal 1,@a.list.size
+  end
 
-#after 
-assert_equal 0,@a.pending.size
-assert_equal 1,@a.completed.size
-assert_equal 1,@a.list.size
- end
 def test_delete
-# precondition
-@a.empty
-@a.add("one")
- @a.complete(1)
+  # precondition
+  @a.empty
+  @a.add("one")
+  @a.complete(1)
 
-#before state
+ #before state
 assert_equal 0,@a.pending.size
 assert_equal 1,@a.completed.size
 assert_equal 1,@a.list.size
-
   @a.delete(1)
-#after delition
    assert_equal 0,@a.pending.size
    assert_equal 0,@a.completed.size
    assert_equal 0,@a.list.size
  end
 
  def test_modify
-#before state
    @a.empty
-   @a.add("write")
-   @a.add("read")
-   assert_equal 2,@a.list.size
-   assert_equal 2,@a.pending.size
-   assert_equal "merge",@a.modify(2,"merge")
-#after merge
-   assert_equal 2,@a.pending.size
-   assert_equal "merge",@a.show_pending(2)
+#precondition
+   assert_equal 0,@a.pending.size
    assert_equal 0,@a.completed.size
-#before complition
-@a.complete(1)
-#after complition
-assert_equal 1,@a.completed.size
-assert_equal "write",@a.show_completed(1)
-end
-def test_load
-@a.empty
-#after loading
-   @a.load1
+   assert_equal 0,@a.list.size
+   @a.add("one")
+   @a.add("two")
+   @a.add("three")
+   assert_equal 3,@a.pending.size
+   assert_equal 0,@a.completed.size
+   assert_equal 3,@a.list.size
+   
+   assert_equal "merge",@a.modify(3,"merge")
+   assert_equal "read",@a.modify(2,"read")
+   assert_equal 3,@a.pending.size
+   assert_equal 0,@a.completed.size
+   assert_equal 3,@a.list.size
+
+  @a.complete(1)
    assert_equal 2,@a.pending.size
    assert_equal 1,@a.completed.size
    assert_equal 3,@a.list.size
-   @a.save
+ end
+
+  def test_pend
+    @a.empty
+    assert_equal 0,@a.pending.size
+   assert_equal 0,@a.completed.size
+   assert_equal 0,@a.list.size
+    @a.add("hi ")
+    @a.add("hello ")
+    @a.add("hwru ")
+   assert_equal 3,@a.pending.size
+   @a.complete(2)
+    assert_equal 2,@a.pending.size
+   assert_equal 1,@a.completed.size
+   assert_equal 3,@a.list.size
+   
+  end
+
+  def test_qcompleted
+
+   @a.empty
+    assert_equal 0,@a.pending.size
+   assert_equal 0,@a.completed.size
+   assert_equal 0,@a.list.size
+    @a.add("google ")
+    @a.add("empty")
+    @a.add("yahoo ")
+   assert_equal 3,@a.pending.size
+   @a.complete(2)
+    assert_equal 2,@a.pending.size
+   assert_equal 1,@a.completed.size
+   assert_equal 3,@a.list.size
+ 
+ end
+  def test_read
+       @a.empty
+       @a.add("veer #undone")
+       @a.add("yh #undone")
+     assert_equal 2,@a.pending.size
+     assert_equal 0,@a.completed.size
+      @a.complete9(1)
+     assert_equal 1,@a.pending.size
+     assert_equal 1,@a.completed.size
+     assert_equal 2,@a.list.size 
+   assert_equal 2,@a.load1.size
+  end
+ 
+ def test_save1
+  @a.empty
+     @a.add("man #undone")
+       @a.add("sag #undone")
+     assert_equal 2,@a.pending.size
+     assert_equal 0,@a.completed.size
+      @a.complete9(1)
+     assert_equal 1,@a.pending.size
+     assert_equal 1,@a.completed.size
+     assert_equal 2,@a.list.size 
+     assert_equal 2,@a.save.size
+ end
+   
+  def test_save_to_file
+    @a.empty
+    @a.add("veeresh #undone")
+       @a.add("yh #undone")
+     assert_equal 2,@a.pending.size
+     assert_equal 0,@a.completed.size
+      @a.complete9(1)
+     assert_equal 1,@a.pending.size
+     assert_equal 1,@a.completed.size
+     assert_equal 2,@a.list.size 
+     assert_equal 2,@a.save.size
+     @a.save_to_file("veer.txt")
+    
 end
+ def test_write_file
+    @a.empty
+  assert_equal 1,@a.load_from_file("veer.txt").size
+    assert_equal 2,@a.list.size 
+ end
 end
